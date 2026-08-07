90a6f924e0841afa8050e02948094dd518b96d52
yashah1707 <yashah1707@gmail.com>
2026-08-07 21:26:42 +0530

    functions

---
+++ src/Functions/Sum.java
@@ -0,0 +1,23 @@
+package Functions;
+import java.util.*;
+
+
+public class Sum {
+    public static void main(String[] args) {
+
+        sum();
+    }
+
+    static void sum(){
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter number 1: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2 = in.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+    }
+}
